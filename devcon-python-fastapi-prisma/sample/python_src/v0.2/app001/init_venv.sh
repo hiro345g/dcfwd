4d34ad591dcfc9b192b155ce8769c59d923f5f3e
hiro345g <hiro345g@gmail.com>
2024-06-20 08:37:43 +0900

    poetry env remove "${current_venv}" への修正

--- devcon-python-fastapi-prisma/sample/python_src/v0.2/app001/init_venv.sh
+++ devcon-python-fastapi-prisma/sample/python_src/v0.2/app001/init_venv.sh
@@ -3,7 +3,7 @@
 # 現在の Python 仮想環境があるなら、それを削除
 current_venv=$(poetry env list | awk '{print $1}')
 if [ "x${current_venv}" != "x" ]; then
-    poetry env remove ${current_venv}
+    poetry env remove "${current_venv}"
 fi
 
 # プロジェクト直下に Python 仮想環境を作成
