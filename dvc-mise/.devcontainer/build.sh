3a247c8774fb51b7ae058a2d7400474adb30b2b1
hiro345g <hiro345g@gmail.com>
2025-12-17 18:38:39 +0900

    dvc-mise/.devcontainer/build.sh で dvc-mise:base のビルドができていなかったので修正

--- dvc-mise/.devcontainer/build.sh
+++ dvc-mise/.devcontainer/build.sh
@@ -1,13 +1,17 @@
 #! /bin/sh
 SCRIPT_DIR=$(dirname "$0")
 BUILD_DIR=$(cd "${SCRIPT_DIR}" || exit 1;pwd)
+BASE_BUILD_DIR=$(cd "${SCRIPT_DIR}"/../build-base-image || exit 1;pwd)
 
-if docker image ls | grep dvc-mise-base > /dev/null; then
-  if [ ! -e "${BUILD_DIR}/build-base-image/build.sh" ]; then
-    echo "build dvc-mise-base image"
+if docker image ls --format "{{.Repository}}:{{.Tag}}" | grep "dvc-mise:base" > /dev/null; then
+  :
+else
+  if [ ! -e "${BASE_BUILD_DIR}/build.sh" ]; then
+    echo "Please build dvc-mise:base image"
     exit 1
   fi
-  bash "${BUILD_DIR}/build-base-image/build.sh"
+  echo "---- building dvc-mise:base image"
+  bash "${BASE_BUILD_DIR}/build.sh"
 fi
 
 # shellcheck disable=SC2068
